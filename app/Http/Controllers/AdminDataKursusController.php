4c3c5db08d04ad9b7ee9537a158898e6c0076741
HANIF FEBRIANSYAH <shundallbolong@gmail.com>
2024-08-27 23:53:31 +0700

    Perbaiki Controller

--- app/Http/Controllers/AdminDataKursusController.php
+++ app/Http/Controllers/AdminDataKursusController.php
@@ -7,32 +7,46 @@ use Illuminate\Http\Request;
 
 class AdminDataKursusController extends Controller
 {
-    public function index()
+    public function dataKursus()
     {
         $courses = DataKursus::all();
         return view('admin.dataKursusAdmin', ['courses' => $courses]);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('admin.tambahDataKursusAdmin');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate(
-            [
-
-            ]
+            []
         );
     }
 
-    public function detail(){
+    public function detail()
+    {
         $courses = DataKursus::all();
         return view('user.detailKursus', compact('courses'));
     }
 
-    public function destroy(string $id){
+    public function destroy(string $id)
+    {
         $data = DataKursus::find($id);
         $data->delete();
     }
 
+    public function home()
+    {
+        return view('user.home');
+    }
+    public function kursus()
+    {
+        return view('user.kursus');
+    }
+    public function maps()
+    {
+        return view('user.peta');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\AdminDataKursusController;
-use App\Http\Controllers\AdminTambahDataKursusController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\UserKursusController;
-use App\Http\Controllers\UserPetaController;
-use App\Http\Controllers\UserDetailDataKursusController;
-use App\Http\Controllers\UserHomeController;
-use App\Models\DataKursus;
+
+
 
 Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.home');
-Route::get('/admin/data-kursus', [AdminDataKursusController::class, 'index'])->name('admin.dataKursus');
+Route::get('/admin/data-kursus', [AdminDataKursusController::class, 'dataKursus'])->name('admin.dataKursus');
 Route::get('/admin/tambahdata', [AdminDataKursusController::class, 'create'])->name('admin.tambahDataKursus');
 
 
@@ -21,9 +17,11 @@ Route::get('/admin/courses', [AdminDataKursusController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 // Route::post('/login', [LoginController::class, 'login'])->name('login.post');
-Route::get('/', [UserHomeController::class, 'index'])->name('home');
-Route::get('/kursus', [UserKursusController::class, 'index'])->name('user.kursus'); // Perbaikan pada rute ini
-Route::get('/peta', [UserPetaController::class, 'index'])->name('user.peta');
+
+
+Route::get('/', [AdminDataKursusController::class, 'home'])->name('home');
+Route::get('/kursus', [AdminDataKursusController::class, 'kursus'])->name('user.kursus'); // Perbaikan pada rute ini
+Route::get('/peta', [AdminDataKursusController::class, 'maps'])->name('user.peta');
 Route::get('/detailkursus', [AdminDataKursusController::class, 'detail'])->name('user.detailKursus');
 Route::delete('/admin/delete/{id}', [AdminDataKursusController::class, 'destroy'])->name('delete');
 
